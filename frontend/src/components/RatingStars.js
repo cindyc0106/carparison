7e0a9211b14b2bfeebef5b9ccc44dc65178c9af3
Cindy Chen <cindy.c0106@gmail.com>
2022-10-29 07:54:13 -0600

    adding stars dynamic state

--- frontend/src/components/RatingStars.js
+++ frontend/src/components/RatingStars.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { FaStar } from "react-icons/fa";
 // import "components/RatingStars.css";
 
 function RatingStars() {
+
+  const [rating, setRating] = useState(null)
+
+//create an array of 5 elements and map over to insert each "star"
   const stars = [...Array(5)].map((star, index) => {
+    //giving the radio input a value for each star
     const value = index + 1;
     return (
       <label>
-        <input type="radio" value={value}/>
+        <input type="radio" value={value} onClick={()=> setRating(value)}/>
         <FaStar size={25} className="pointer" />
       </label>
     );
   });
+
 
   return <div className="stars">{stars}</div>;
 }
