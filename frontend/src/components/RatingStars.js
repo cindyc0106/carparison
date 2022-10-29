dbd45bdd9862baec33bea407459ee66438c105bc
Cindy Chen <cindy.c0106@gmail.com>
2022-10-29 09:06:13 -0600

    added hover state and color/styling

--- frontend/src/components/RatingStars.js
+++ frontend/src/components/RatingStars.js
@@ -5,6 +5,7 @@ import { FaStar } from "react-icons/fa";
 function RatingStars() {
 
   const [rating, setRating] = useState(null)
+  const [hover, setHover] = useState(null)
 
 //create an array of 5 elements and map over to insert each "star"
   const stars = [...Array(5)].map((star, index) => {
@@ -12,8 +13,18 @@ function RatingStars() {
     const value = index + 1;
     return (
       <label>
-        <input type="radio" value={value} onClick={()=> setRating(value)}/>
-        <FaStar size={25} className="pointer" />
+        <input 
+          type="radio" 
+          value={value} 
+          onClick={() => setRating(value)}
+        />
+          <FaStar 
+            size={25} 
+            className="pointer" 
+            color={ value <= (hover || rating) ? "#f9d02f": "#c7c7c7"}
+            onMouseEnter={() => setHover(value)}
+            onMouseLeave={() => setHover(null)}
+          />
       </label>
     );
   });
