a0cefcef420394b0972bdbe5e60a21f7e50e1e57
Kevin Ly <ly1@ualberta.ca>
2022-10-28 18:05:19 -0600

    get server working

--- backend-carparison/app.js
+++ backend-carparison/app.js
@@ -3,12 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const db = require('./db');
-const dbHelpers = require('./helpers/dbHelpers')(db);
+// const dbHelpers = require('./helpers/dbHelpers')(db);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
+const PORT = 3001; 
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,7 +17,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter(dbHelpers));
+// app.use('/', indexRouter);
+// app.use('/api/users', usersRouter(dbHelpers));
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}!`);
+});
 
 module.exports = app;
