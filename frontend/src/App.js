6ebb8c7a6f1e8b67df8c0bf6e6ab9b18329a903d
Kevin Ly <ly1@ualberta.ca>
2022-10-28 20:49:17 -0600

    create init nav layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 
 import './App.css';
-import Navigator from './components/Navigator'
-import Review from './components/Review'
-import Car from './components/Car'
+import Navigator from './components/Navigator';
+import Review from './components/Review';
+import Car from './components/Car';
 import RatingStars from './components/RatingStars';
 import PastReviewList from './components/PastReviewList';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-     <ChakraProvider>
-    <div className="App">
-      < Navigator />
-      < Review />
-      < Car />
-      < RatingStars />
-      < PastReviewList />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <nav className='Navigator'>
+          < Navigator />
+        </nav>
+        < Review />
+        < Car />
+        < RatingStars />
+        < PastReviewList />
+      </div>
     </ChakraProvider>
   );
 }
--- frontend/src/components/Navigator.js
+++ frontend/src/components/Navigator.js
@@ -1,7 +1,125 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Input,
+  Button
+} from '@chakra-ui/react';
+
+import { useState } from 'react';
+
+data = [
+  {
+    "city_mpg": 23,
+    "class": "compact car",
+    "combination_mpg": 24,
+    "cylinders": 4,
+    "displacement": 1.6,
+    "drive": "fwd",
+    "fuel_type": "gas",
+    "highway_mpg": 26,
+    "make": "toyota",
+    "model": "corolla",
+    "transmission": "a",
+    "year": 1993
+  },
+  {
+    "city_mpg": 23,
+    "class": "compact car",
+    "combination_mpg": 26,
+    "cylinders": 4,
+    "displacement": 1.6,
+    "drive": "fwd",
+    "fuel_type": "gas",
+    "highway_mpg": 31,
+    "make": "toyota",
+    "model": "corolla",
+    "transmission": "m",
+    "year": 1993
+  },
+  {
+    "city_mpg": 23,
+    "class": "compact car",
+    "combination_mpg": 25,
+    "cylinders": 4,
+    "displacement": 1.8,
+    "drive": "fwd",
+    "fuel_type": "gas",
+    "highway_mpg": 30,
+    "make": "toyota",
+    "model": "corolla",
+    "transmission": "a",
+    "year": 1993
+  },
+  {
+    "city_mpg": 23,
+    "class": "compact car",
+    "combination_mpg": 26,
+    "cylinders": 4,
+    "displacement": 1.8,
+    "drive": "fwd",
+    "fuel_type": "gas",
+    "highway_mpg": 30,
+    "make": "toyota",
+    "model": "corolla",
+    "transmission": "m",
+    "year": 1993
+  },
+  {
+    "city_mpg": 18,
+    "class": "midsize car",
+    "combination_mpg": 21,
+    "cylinders": 4,
+    "displacement": 2.2,
+    "drive": "fwd",
+    "fuel_type": "gas",
+    "highway_mpg": 26,
+    "make": "toyota",
+    "model": "camry",
+    "transmission": "a",
+    "year": 1993
+  }
+];
+
 function Navigator() {
+  const [query, setQuery] = useState("");
+
   return (
-    <h1> This is the Nav </h1>
-    
+    <nav>
+      <div className='nav'>
+        <h1> LOGO </h1>
+        <form className='searchbar'>
+
+          <Input placeholder='Search for your car' onChange={e => setQuery(e.target.value)} />
+
+          <Button colorScheme='teal' variant='outline'>Submit</Button>
+          <Button isLoading
+            loadingText='Loading'
+            colorScheme='teal'
+            variant='outline'
+            spinnerPlacement='end'
+          >
+            Submit</Button>
+
+        </form>
+
+        <Breadcrumb separator='-'>
+          <BreadcrumbItem>
+            <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+          </BreadcrumbItem>
+
+          <BreadcrumbItem>
+            <BreadcrumbLink href='#'>About</BreadcrumbLink>
+          </BreadcrumbItem>
+
+          <BreadcrumbItem isCurrentPage>
+            <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+
+      </div>
+    </nav>
+
   );
 }
 
