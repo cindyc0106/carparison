163d1f2552c5638de63cc9489e4f16d8d4fe01b5
Kevin Ly <ly1@ualberta.ca>
2022-10-30 14:51:53 -0600

    add car database for filtering

--- frontend/src/components/CarSearch.js
+++ frontend/src/components/CarSearch.js
@@ -18,15 +18,8 @@ function CarSearch() {
     axios.get("http://localhost:3001/cars")
       .then((res) => JSON.parse(JSON.stringify(res)))
       .then((data) => {
-        console.log(data.data);
         const carObj = data.data;
         setCars(carObj)
-        // carObj.map((car) => {
-        //   setMake(car.make);
-        //   setModel(car.model);
-        //   setYear(car.year);
-        //   return data.data;
-        // });
       });
   }, []);
   
@@ -35,27 +28,21 @@ function CarSearch() {
       <FormControl>
         <FormLabel>Make</FormLabel>
         <Select placeholder="Select Make">
-          {/* <option>{make}</option> */}
           {cars.map((c, key) => <option key={key}>{c.make}</option>)}
-          <option>Nigeria</option>
         </Select>
       </FormControl>
 
       <FormControl>
         <FormLabel>Model</FormLabel>
         <Select placeholder="Select Model">
-          {/* <option>{model}</option> */}
           {cars.map((c, key) => <option key={key}>{c.model}</option>)}
-          <option>Nigeria</option>
         </Select>
       </FormControl>
 
       <FormControl>
         <FormLabel>Year</FormLabel>
         <Select placeholder="Select Year">
-          {/* <option>{year}</option> */}
           {cars.map((c, key) => <option key={key}>{c.year}</option>)}
-          <option>Nigeria</option>
         </Select>
       </FormControl>
     </div>
