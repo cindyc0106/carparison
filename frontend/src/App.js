c19c23f307d16cad400221f0552399f2fad9a3b4
Cindy Chen <cindy.c0106@gmail.com>
2022-10-31 17:25:07 -0600

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import Navigator from "./components/Navigator";
-import Review from "./components/Review";
+// import Review from "./components/Review";
 import { ChakraProvider } from "@chakra-ui/react";
 import CarSearch from "./components/CarSearch";
-import PastReviewList from "./components/PastReviewList";
+// import PastReviewList from "./components/PastReviewList";
 
 function App() {  
 
@@ -22,7 +22,7 @@ requestAnimationFrame(draw);
       <div className="foreground"> </div>
       <div className="App">
 
-        
+   
       <CarSearch />
 
     {/* <Review /> <PastReviewList id={}/> */}
