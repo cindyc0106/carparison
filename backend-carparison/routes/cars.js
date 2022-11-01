532ed1d553b64a3d1a809cd331f469cec7f5409f
Kevin Ly <ly1@ualberta.ca>
2022-11-01 15:29:40 -0600

    fix api key

--- backend-carparison/routes/cars.js
+++ backend-carparison/routes/cars.js
@@ -3,14 +3,11 @@ const router = express.Router();
 const db = require('../db');
 // db.connect();
 const axios = require("axios");
+require('dotenv').config();
+
 
 // (db) => {
   router.get('/', (req, res) => {
-  console.log("main page:", req.body)
-    // console.log('req.params', req.params)
-    // console.log('req.body', req.body)
-    
-   
 
     return db.query(
       `SELECT * FROM cars;`
@@ -33,15 +30,15 @@ const axios = require("axios");
       url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
       params: {make: `${req.params.make}`, model: `${req.params.model}`, year: `${req.params.year}`},
       headers: {
-        'X-RapidAPI-Key': '9f187eb2d6mshc202d7cdb328f77p1e39c4jsn555dae14060a',
+        'X-RapidAPI-Key': `${process.env.API_KEY}`,
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
       }
     };
     
     return axios.request(options)
     .then(function (response) {
-      console.log("response data:", response.data);
-      res.json(response.data)
+      console.log("response data:", response.data[0]);
+      res.json(response.data[0])
     }).catch(function (error) {
       console.error(error);
     });
