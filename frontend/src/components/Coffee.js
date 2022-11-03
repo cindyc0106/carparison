f812e81936966a30e42548f6d5fb44ce5b993c94
Cindy Chen <cindy.c0106@gmail.com>
2022-11-03 13:45:59 -0600

    adding Coffee button

--- frontend/src/components/Coffee.js
+++ frontend/src/components/Coffee.js
@@ -1,23 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./Coffee.css";
 import {
-Alert
+  Button,
 } from "@chakra-ui/react";
 
 const ProductDisplay = () => (
   <section>
-    <div className="product">
-      <img
-        src="https://i.imgur.com/EHyR2nP.png"
-        alt="The cover of Stubborn Attachments"
-      />
-      <div className="description">
-        <h3>Stubborn Attachments</h3>
-        <h5>$20.00</h5>
-      </div>
-    </div>
     <form action="/create-checkout-session" method="POST">
-      <button type="submit">Checkout</button>
+      <Button className="coffee-button" type="submit">
+        Buy us a coffee!
+      </Button>
     </form>
   </section>
 );
@@ -36,18 +28,19 @@ export default function Coffee() {
     const query = new URLSearchParams(window.location.search);
 
     if (query.get("success")) {
-      <Alert status="success">
-        <AlertIcon />
-        Thank you for buying us a coffee!
-      </Alert>
+      setMessage("Thank you for buying us a coffee :)");
     }
 
     if (query.get("canceled")) {
       setMessage(
-        "Order canceled -- continue to shop around and checkout when you're ready."
+        "Order canceled -- please try again"
       );
     }
   }, []);
 
-  return message ? <Message message={message} /> : <ProductDisplay />;
+  return message ? (
+    <Message message={message} />
+  ) : (
+    <ProductDisplay />
+  );
 }
