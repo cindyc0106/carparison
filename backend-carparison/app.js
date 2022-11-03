1e76cd6b18aebc3c115f649d7f8f9e41ee786a66
Cindy Chen <cindy.c0106@gmail.com>
2022-11-03 13:40:48 -0600

    adding Coffee Component for Stripe

--- backend-carparison/app.js
+++ backend-carparison/app.js
@@ -3,40 +3,15 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
-require('dotenv').config()
 
+const stripe = require('stripe')('sk_test_51LuJmfKPWu57wspdmBBDXEeYwpfrPpPUhx5SqNw8AP0p3g7D4G97CHUlFF9RUdt91qLHbX1vHh3knRDCj0RLT0mi00EX7p5Zz2');
+// const dbHelpers = require('./helpers/dbHelpers')(db);
+
+// var indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const carsRouter = require('./routes/cars');
 const reviewsRouter = require('./routes/reviews');
 const listsRouter = require('./routes/cars_lists');
-
-const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
-
-const items = new Map([[
-  1, { priceInCents: 250, name: "Buy a coffee" }
-]]) 
-
-const button = document.querySelector("button")
-button.addEventListener("click", () => {
-  fetch('/checkout'), {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      items: [
-        { id: 1, quantity: 1}
-      ]
-    }).then(res => {
-      if(res.ok) return res.json()
-      return res.json().then(json => Promise.reject(json))
-    }).then(({url}) => {
-      window.location = url
-    }).catch(err => {
-      console.log(err.error)
-    })
-  }
-})
 
 const app = express();
 const PORT = 3001; 
@@ -46,42 +21,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 app.use('/cars', carsRouter);
 app.use('/reviews', reviewsRouter);
 app.use('/cars_lists', listsRouter);
 
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 'price_1M079aKPWu57wspdTEO7uj6I',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${process.env.CLIENT_URL}?success=true`,
+    cancel_url: `${process.env.CLIENT_URL}?canceled=true`,
+  });
+
+  res.redirect(303, session.url);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}!`);
 });
-
-app.post("/checkout", async (req, res) => {
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'], 
-      mode: 'payment',
-      line_items: req.body.items.map(item => {
-        const storeItem = items.get(item.id)
-        return {
-          price_data: {
-            currency: 'cad',
-            product_data: {
-              name: storeItem.name
-            }
-          },
-          quantity: item.quantity
-        }
-      }),
-      success_url: `${process.env.SERVER_URL}/success.html` ,
-      cancel_url: `${process.env.SERVER_URL}/cancel.html` 
-    })
-    res.json({url: session.url})
-  } catch (err) {
-    res.status(500).json({error: err.message})
-  }
-})
 
 module.exports = app;
---
+++ frontend/src/components/Coffee.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import "./Coffee.css";
+import {
+Alert
+} from "@chakra-ui/react";
+
+const ProductDisplay = () => (
+  <section>
+    <div className="product">
+      <img
+        src="https://i.imgur.com/EHyR2nP.png"
+        alt="The cover of Stubborn Attachments"
+      />
+      <div className="description">
+        <h3>Stubborn Attachments</h3>
+        <h5>$20.00</h5>
+      </div>
+    </div>
+    <form action="/create-checkout-session" method="POST">
+      <button type="submit">Checkout</button>
+    </form>
+  </section>
+);
+
+const Message = ({ message }) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
+
+export default function Coffee() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get("success")) {
+      <Alert status="success">
+        <AlertIcon />
+        Thank you for buying us a coffee!
+      </Alert>
+    }
+
+    if (query.get("canceled")) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
+  return message ? <Message message={message} /> : <ProductDisplay />;
+}
