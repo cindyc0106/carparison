963c263a567bc107b1ece49273172905935a124b
Kevin Ly <ly1@ualberta.ca>
2022-11-04 17:30:18 -0600

    initial aboutus page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,9 +1,45 @@
-import './styles/About.css'
+import './styles/About.css';
+import { Divider, Stack, Text } from '@chakra-ui/react';
+import { FaLinkedin } from "react-icons/fa";
+
+
+const display1 = (
+  <>
+    <Stack direction='column' h='100px' p={4}>
+      <Divider orientation='vertical' />
+      <Text>Cindy Chen</Text>
+      <FaLinkedin />
+    </Stack>
+  </>
+);
+
+const display2 = (
+  <>
+    <Stack direction='row' h='100px' p={4}>
+      <Divider orientation='vertical' />
+      <Text>Omid Ghahramani</Text>
+      <FaLinkedin />
+    </Stack>
+  </>
+);
+
+const display3 = (
+  <>
+    <Stack direction='row' h='100px' p={4}>
+      <Divider orientation='vertical' />
+      <Text>Kevin Ly</Text>
+      <FaLinkedin />
+    </Stack>
+  </>
+);
+
 
 function About() {
   return (
     <div className="about">
-      <h1>THIS IS THE ABOUT US PAGE</h1>
+      {display1}
+      {display2}
+      {display3}
     </div>
   );
 }
