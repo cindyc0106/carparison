eac961b995956a7d2eec7a4960a6d8927ee5b7fe
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-11-08 18:37:34 -0800

    new seed for review

--- backend-carparison/db/seeds/03_reviews.sql
+++ backend-carparison/db/seeds/03_reviews.sql
@@ -4,6 +4,7 @@ reviews (rating, description, user_name, car_make, car_model, car_year)
 VALUES
     (1, 'Why Lamborghini?', 'Ferrari Enzo', 'lamborghini', 'urus', 2019),
     (4, 'Great car for 150k!!!', 'Lambo Enthusiast 123', 'lamborghini', 'urus', 2019),
+    (2, 'Not worth 150k!!!', 'Lambo Hater 123', 'lamborghini', 'urus', 2019),
     (5, 'What a sexy car', 'Yiqi Zhang', 'lamborghini', 'urus', 2019),
     (5, 'The coupe is ********!', 'Rich the Kid', 'lamborghini', 'urus', 2019),
     (2, 'This car is meh!', 'Test', 'toyota', 'corolla', 2015),
